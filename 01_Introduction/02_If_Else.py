a3c4fc086e596abbbf60850092b152e9b884e9ff
vibhorsingh11 <vibhorsinghcs@gmail.com>
2021-03-06 18:45:48 +0530

    Added 02 If Else

---
+++ 01_Introduction/02_If_Else.py
@@ -0,0 +1,20 @@
+"""
+Given an integer, , perform the following conditional actions:
+
+If  is odd, print Weird
+If  is even and in the inclusive range of  to , print Not Weird
+If  is even and in the inclusive range of  to , print Weird
+If  is even and greater than , print Not Weird
+"""
+
+n = int(input("Enter a number\n"))
+
+if n % 2 != 0:
+    print("Weird")
+else:
+    if 2 <= n <= 5:
+        print("Not Weird")
+    elif 6 <= n <= 20:
+        print("Weird")
+    else:
+        print("Not Weird")
