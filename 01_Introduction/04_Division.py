ee7e88b2ea75cfcf22c308a279fade0fad3b8ddf
vibhorsingh11 <vibhorsinghcs@gmail.com>
2021-03-06 18:53:49 +0530

    Added 04 Division

---
+++ 01_Introduction/04_Division.py
@@ -0,0 +1,14 @@
+"""
+The provided code stub reads two integers,  and , from STDIN.
+
+Add logic to print two lines. The first line should contain the result of integer division, a // b 
+The second line should contain the result of float division,  / .
+
+No rounding or formatting is necessary.
+"""
+if __name__ == '__main__':
+    a = int(input("Enter first Number\n"))
+    b = int(input("Enter second Number\n"))
+
+print(a // b)
+print(a / b)
