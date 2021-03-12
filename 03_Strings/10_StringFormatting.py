6df2f00bb53c6a92144f2b3dc232f2ace16509e9
vibhorsingh11 <vibhorsinghcs@gmail.com>
2021-03-12 19:21:50 +0530

    Added 10 String Formatting

---
+++ 03_Strings/10_StringFormatting.py
@@ -0,0 +1,45 @@
+"""
+Given an integer, n, print the following values for each integer i from l to n:
+
+Decimal
+Octal
+Hexadecimal (capitalized)
+Binary
+The four values must be printed on a single line in the order specified above for each i from l to n. 
+Each value should be space-padded to match the width of the binary value of n.
+Sample Input
+
+17
+Sample Output
+
+    1     1     1     1
+    2     2     2    10
+    3     3     3    11
+    4     4     4   100
+    5     5     5   101
+    6     6     6   110
+    7     7     7   111
+    8    10     8  1000
+    9    11     9  1001
+   10    12     A  1010
+   11    13     B  1011
+   12    14     C  1100
+   13    15     D  1101
+   14    16     E  1110
+   15    17     F  1111
+   16    20    10 10000
+   17    21    11 10001  
+"""
+
+
+def print_formatted(number):
+    # your code goes here
+    w = len("{0:b}".format(number))
+    for i in range(1, number + 1):
+        print("{0:{width}d} {0:{width}o} {0:{width}x} {0:{width}b}".format(
+            i, width=w).upper())
+
+
+if __name__ == '__main__':
+    n = int(input())
+    print_formatted(n)
