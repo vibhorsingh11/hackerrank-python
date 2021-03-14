94e9e96ea1108a9261a2bb2119829158159af11b
vibhorsingh11 <vibhorsinghcs@gmail.com>
2021-03-14 22:25:11 +0530

    Added 06 Unioin

---
+++ 04_Sets/06_Unioin.py
@@ -0,0 +1,15 @@
+"""
+The students of District College have subscriptions to English and French newspapers. Some students have subscribed only to English, 
+some have subscribed to only French and some have subscribed to both newspapers.
+
+You are given two sets of student roll numbers. One set has subscribed to the English newspaper, and the other set is subscribed to the
+French newspaper. The same student could be in both sets. Your task is to find the total number of students who have subscribed to at 
+least one newspaper.
+"""
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+n = int(input())
+nSub = set(map(int, input().split()))
+b = int(input())
+bSub = set(map(int, input().split()))
+unionSub = nSub.union(bSub)
+print(len(unionSub))
