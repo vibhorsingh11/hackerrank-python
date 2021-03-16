f13c50828cb266bc6ba4234576f47a2eb30e472f
vibhorsingh11 <vibhorsinghcs@gmail.com>
2021-03-16 21:51:30 +0530

    Added 10 Mutations

---
+++ 04_Sets/10_Mutations.py
@@ -0,0 +1,11 @@
+# You are given a set A and N number of other sets. These N number of sets have to perform some specific mutation operations on set A.
+
+# Your task is to execute those operations and print the sum of elements from set A.
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+input()
+L = set(input().split())
+for _ in range(int(input())):
+    command, *args = input().split()
+    getattr(L, command)(set(input().split()))
+print(sum(map(int, L)))
